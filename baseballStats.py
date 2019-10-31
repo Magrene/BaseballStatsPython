534a44f5f717bb43ede51960607bdb4d7456829f
Anthony Magrene <ekfhalomaster@gmail.com>
2019-10-31 11:57:38 -0400

    Update baseballstats.py
    Work in progress

--- baseballStats.py
+++ baseballStats.py
@@ -7,19 +7,44 @@ def getPlayerInfo(active, Firstname, Lastname):
     pIDreponsePart1 = "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='"
     pIDreponsePart2 = "'&name_part='"
     pIDreponsePart3 = "%25'"
+    
     pIDresponse = requests.get(pIDreponsePart1 + active + pIDreponsePart2 + Firstname +" " + Lastname + pIDreponsePart3)
+    print(pIDreponse.status_code)
     return pIDresponse
     
 
+def getPlayerCareerStat(active, Firstname, Lastname):
+    pIDresponse=getPlayerInfo(active,Firstname,Lastname)
+    print(pIDresponse.json())
+    print("What would you like to know batting or pitching stats?")
+    typeStat = str(input("0 - Batting stats \n1 - Hitting Stats\n"))
+    if typeStat == '0':
+        pBat = requests.get("http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27"+pIDresponse.json()['search_player_all']['queryResults']['row']['player_id']+"%27")
+        print("What stat would you like to know about?\nSupported Stats\n-------------\n0-Homeruns\n1-Batting Average\n2-RBI\n3-Stolen Bases")
+        pBatS = str(input("Input desired stat type\n"))
+        if pBatS == "0":
+            return pBat.json()['sport_career_hitting']['queryResults']['row']['hr']
+        elif pBatS == "1":
+            return pBat.json()['sport_career_hitting']['queryResults']['row']['avg']
+        elif pBatS == "2":
+           return pBat.json()['sport_career_hitting']['queryResults']['row']['rbi']
+        elif pBatS == "3":
+            return pBat.json()['sport_career_hitting']['queryResults']['row']['sb']
+
 print("Please follow proper formating for all API requests. The text within the () will provide instructions for each request.\n")
-active = str(input("Is the player active? (Y or N)\n")).upper()
-Firstname = str(input("What is the players first name? (firstname)\n"))
-Lastname = str(input("What is the players first name? (lastname)\n"))
+mode = str(input("Are you going to building a table or just looking for a speific stat?\n0-Looking for player stat\n1-Building a Table\n"))
+if mode == '0':
+    active = str(input("Is the player active? (Y or N)\n")).upper()
+    Firstname = str(input("What is the players first name? (firstname)\n"))
+    Lastname = str(input("What is the players last name? (lastname)\n"))
+    print("uh")
+    getPlayerCareerStat(active,Firstname,Lastname)
+    
 
-pIDresponse=getPlayerInfo(active,Firstname,Lastname)
 
 
-print(pIDresponse.json())
+
+
 
 print("What would you like to know about this player batting or hitting stats?")
 typeStat = str(input("0 - Batting stats \n1 - Hitting Stats\n"))
@@ -39,6 +64,10 @@ if(typeStat == '0'):
     
     
 
+
+#active = str(input("Is the player active? (Y or N)\n")).upper()
+#Firstname = str(input("What is the players first name? (firstname)\n"))
+#Lastname = str(input("What is the players first name? (lastname)\n"))
 
 #http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code=%27mlb%27&active_sw=%27N%27&name_part=%27henderson%25%27
 #"http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code=%27mlb%27&active_sw=%27"+ active + "name_part=%27" +Firstname+ "%20" + Lastname + "%25%27"
