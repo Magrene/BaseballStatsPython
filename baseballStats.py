a8fd840d3127844e73576797a074f289002dadc5
Anthony Magrene <ekfhalomaster@gmail.com>
2019-11-12 13:34:48 -0500

    Update baseballStats.py
    *Corrections to input prompts.
    *Work on table functionality.

--- baseballStats.py
+++ baseballStats.py
@@ -1,5 +1,5 @@
 import requests
-import pandas as pd
+
 import json
 
 def getPlayerID():
@@ -13,23 +13,21 @@ def getPlayerID():
     Firstname = str(input("What is the players first name? (firstname)\n"))
     Lastname = str(input("What is the players last name? (lastname)\n"))
     pIDresponse = requests.get(pIDreponsePart1 + active + pIDreponsePart2 + Firstname +" " + Lastname + pIDreponsePart3)
-    return pIDresponse
+    return pIDresponse.json()['search_player_all']['queryResults']['row']['player_id']
     
-def userIntStatType(active, Firstname, Lastname):
+def userIntStatType():
+    global pBatS
+    global pBat
     print("What would you like to know batting or pitching stats?")
-    typeStat = str(input("0 - Batting stats \n1 - Hitting Stats\n"))
+    typeStat = str(input("0 - Batting stats \n1 - Pitching Stats\n"))
     if typeStat == '0':
-        pBat = requests.get("http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27"+pIDresponse.json()['search_player_all']['queryResults']['row']['player_id']+"%27")
-        print("What stat would you like to know about?\nSupported Stats\n-------------\n0-Homeruns\n1-Batting Average\n2-RBI\n3-Stolen Bases")
-        pBatS = str(input("Input desired stat type\n"))
+        pBat = requests.get("http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27"+playerID+"%27")
+        print("What stat would you like to know about?\nSupported Stats\n-------------\n0-Homeruns\n1-Batting Average\n2-RBI\n3-Stolen Bases\n4-On Base Percentage")
+        return str(input("Input desired stat type\n"))
     
 def getPlayerCareerHittingStat(playerID):
-    
-    
-    
-    pBat = requests.get("http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id=%27mlb%27&game_type=%27R%27&player_id=%27"+pIDresponse.json()['search_player_all']['queryResults']['row']['player_id']+"%27")
-    print("What stat would you like to know about?\nSupported Stats\n-------------\n0-Homeruns\n1-Batting Average\n2-RBI\n3-Stolen Bases")
-    pBatS = str(input("Input desired stat type\n"))
+    global pBatS
+    pBatS = userIntStatType()
     if pBatS == "0":
         return pBat.json()['sport_career_hitting']['queryResults']['row']['hr']
     elif pBatS == "1":
@@ -38,26 +36,43 @@ def getPlayerCareerHittingStat(playerID):
         return pBat.json()['sport_career_hitting']['queryResults']['row']['rbi']
     elif pBatS == "3":
         return pBat.json()['sport_career_hitting']['queryResults']['row']['sb']
+    elif pBatS == "4":
+        return pBat.json()['sport_career_hitting']['queryResults']['row']['ops']
+def cleanSingleStatPrint(stat,pBatS):
+    if pBatS == "0":
+        print(Firstname + " " + Lastname + " has " + str(stat) + " total home runs.")
+    elif pBatS == "1":
+        print(Firstname + " " + Lastname + " has a batting average of " + str(stat) + " .")
+    elif pBatS == "2":
+        print(Firstname + " " + Lastname + " has " + str(stat) + " total runs batted in.")
+    elif pBatS == "3":
+        print(Firstname + " " + Lastname + " has " + str(stat) + " total stolen bases.")
+    elif pBatS == "4":
+        print(Firstname + " " + Lastname + " has an on base percentage of " + str(stat) + " .")
+    
 
 active = ''
 Firstname= ''
 Lastname = ''
 playerID = ''
-
+pBatS = ''
 print("Please follow proper formating for all API requests. The text within the () will provide instructions for each request.\n")
 mode = str(input("Are you going to building a table or just looking for a speific stat?\n0-Looking for player stat\n1-Building a Table\n"))
 if mode == '0':
-    
     playerID = getPlayerID()
-    getPlayerCareerHittingStat(playerID)
+    stat = getPlayerCareerHittingStat(playerID)
+    cleanSingleStatPrint(stat,pBatS)
+elif mode == '1':
+    playerIDList = []
+    mode = '0'
+    while mode == '0':
+        playerIDList.append(getPlayerID())
+        print("0-Add additonal player to list\n1-Review players you have selected")
+        mode = str(input())
+        if mode == '1':
+            print(playerIDList)
+            mode = str(input('0-Add additonal player to list\n1-Finalize List'))
     
-    
-
-
-
-
-
-
 
 #print(Firstname + " " + Lastname + " has " + str(pBat.json()['sport_career_hitting']['queryResults']['row']['hr']) + " total home runs.")
 #print(Firstname + " " + Lastname + " has a batting average of " + str(pBat.json()['sport_career_hitting']['queryResults']['row']['avg']) + " .")
